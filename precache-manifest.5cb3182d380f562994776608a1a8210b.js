22d33b14a5c1d4ae92b3fc55737659bae3d85be4
Igor Shybyryn <edm0ndantes@ukr.net>
2025-04-25 17:43:45 +0300

    Deploy React app to GitHub Pages

---
+++ precache-manifest.5cb3182d380f562994776608a1a8210b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7336a8dd95b05639bf7f484c0423ae",
+    "url": "/proposal-builder/index.html"
+  },
+  {
+    "revision": "7e833302dada2370062b",
+    "url": "/proposal-builder/static/css/main.c8a76921.chunk.css"
+  },
+  {
+    "revision": "f64fa09e8954fa63efae",
+    "url": "/proposal-builder/static/js/2.a91a2fbf.chunk.js"
+  },
+  {
+    "revision": "7e833302dada2370062b",
+    "url": "/proposal-builder/static/js/main.7c8832a8.chunk.js"
+  },
+  {
+    "revision": "c7a85beb275d0117bcf3",
+    "url": "/proposal-builder/static/js/runtime~main.a5dbe802.js"
+  },
+  {
+    "revision": "977ee1af385f84128980ac0bbe3c817a",
+    "url": "/proposal-builder/static/media/sirmasa logo gray only logo.977ee1af.svg"
+  }
+]);
